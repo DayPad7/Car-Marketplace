67493054cf5ed883de4f6621faa6f30248f547df
DayPad7 <dayanapadilla7@gmail.com>
2023-11-29 15:19:56 -0600

    first commit

---
+++ src/Firebase.config.js
@@ -0,0 +1,17 @@
+import {getFirestore} from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5jg_cd0xu6l6YqxSkRgzJyyNv8NLP9RM",
+  authDomain: "car-marketplace-project.firebaseapp.com",
+  projectId: "car-marketplace-project",
+  storageBucket: "car-marketplace-project.appspot.com",
+  messagingSenderId: "419027441132",
+  appId: "1:419027441132:web:308a0818c163ba49825cc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
